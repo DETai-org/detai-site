e57ab49ec9408d29be56594e2d991be629fe46f5
Anton-Psy <a17527905@gmail.com>
2025-12-04 22:49:40 +0300

    Use static DETai logo on hero

--- components/visual/AnimatedLogo.tsx
+++ components/visual/AnimatedLogo.tsx
@@ -3,12 +3,12 @@
 import Lottie from "lottie-react";
 import clsx from "clsx";
 
-import animationData from "@/public/assets/animations/logo-detai.json";
+import animationData from "@/public/assets/animations/logo-detai-static.json";
 
 type AnimatedLogoProps = {
   className?: string;
 };
 
 export default function AnimatedLogo({ className }: AnimatedLogoProps) {
-  return <Lottie animationData={animationData} loop autoplay className={clsx("h-48 w-48", className)} />;
+  return <Lottie animationData={animationData} loop={false} autoplay className={clsx("h-48 w-48", className)} />;
 }
