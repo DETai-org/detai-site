ba2197e44894c70d2ff1be7e4382ae1542b5e385
Anton-Psy <a17527905@gmail.com>
2025-12-14 00:38:50 +0300

    Update CanvasParticlesLayer.tsx

--- components/visual/CanvasParticlesLayer.tsx
+++ components/visual/CanvasParticlesLayer.tsx
@@ -207,9 +207,14 @@ export default function CanvasParticlesLayer({ className }: CanvasParticlesLayer
     const nvx = (particle.vx + ax) * damping;
     const nvy = (particle.vy + ay) * damping;
 
-    if (nx < -width * 0.1 || nx > width * 1.1 || ny < -height * 0.1 || ny > height * 1.1) {
+    if (nx < -width * 0.5 || nx > width * 1.5 || ny < -height * 0.5 || ny > height * 1.5) {
       return null;
     }
+
+
+    // if (nx < -width * 0.1 || nx > width * 1.1 || ny < -height * 0.1 || ny > height * 1.1) {
+    //   return null;
+    // }
 
     if (distance < Math.min(width, height) * 0.025) {
       particle.opacity *= 0.9; // РАСТВОРЕНИЕ В ЛОГО ❗❗❗
