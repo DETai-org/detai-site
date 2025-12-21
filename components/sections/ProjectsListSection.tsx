1fdc0b97c8e250c59c4a536b9d7a3337c0ca7170
Anton-Psy <a17527905@gmail.com>
2025-12-21 21:25:53 +0300

    Adjust projects carousel container overflow

--- components/sections/ProjectsListSection.tsx
+++ components/sections/ProjectsListSection.tsx
@@ -75,8 +75,8 @@ function EchelonRow({ echelon, items }: { echelon: Project["echelon"]; items: Pr
         </div>
       </div>
 
-      <div className="relative -mx-mobile-4 md:-mx-10">
-        <div className="bg-basic-dark px-mobile-4 pr-8 md:px-10 md:pr-16">
+      <div className="relative -mx-mobile-4 overflow-visible md:-mx-10">
+        <div className="px-mobile-4 pr-8 md:px-10 md:pr-16">
           <div
             ref={scrollRef}
             className="flex gap-mobile-4 overflow-x-auto overflow-y-visible scroll-smooth pb-4 md:gap-6"
